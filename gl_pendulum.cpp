744eeb2283de03c8f3b53949f628478004d1bade
Igor Pejic <igor.pejic@dr.com>
2016-01-17 19:14:16 +0100

    Reorder function prototypes.

--- gl_pendulum.cpp
+++ gl_pendulum.cpp
@@ -18,6 +18,9 @@ GLfloat green[] = { 0.0f, 1.0f, 0.0f, 1.0f };
 GLfloat blue[] = { 0.647, 0.874f, 0.949, 1.0f };
 GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
+bool keyStates[256];
+bool animate = 0;
+
 vector<vector<float>> trail(1, vector<float>(16));
 int trail_index = 0;
 
@@ -26,6 +29,14 @@ vector<map<char, RotateParam>> rotate_axis = {
     {{'x', {false, 0.0, 0}}, {'y', {false, 0.0, 0}}, {'z', {false, 90.0, 0}}}
     };
 
+void drawSphere(void);
+void drawCylinder(void);
+void drawBottomCylinder(void);
+void increaseAngle(void);
+void drawTrail(void);
+void keyOperations(void);
+void keyPressed(unsigned char key, int, int);
+void keyUp (unsigned char key, int, int);
 
 class Rectangle {
 public:
@@ -94,140 +105,7 @@ void Rectangle::draw(void)
     glPopMatrix();
 }
 
-void drawSphere(void) {
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, green);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 20.0);
-    glutSolidSphere(1.0, 30, 30);
-}
 
-void drawCylinder(void) {
-    GLUquadricObj *obj = gluNewQuadric();
-        glTranslatef(0, -1.0, 0);
-        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, blue);
-        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, blue);
-        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
-        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 20.0);
-    glPushMatrix();
-        glRotatef(90, 1.0, 0, 0);
-        gluDisk(obj, 0.0, 1.0, 30, 30);
-        gluCylinder(obj, 1.0, 1, 3, 30, 30);
-        glPopMatrix();
-    glPushMatrix();
-        glTranslatef(0, -3.0, 0);
-        glPushMatrix();
-            glRotatef(-90, 1.0, 0, 0);
-            gluDisk(obj, 0.0, 1.0, 30, 30);
-        glPopMatrix();
-}
-
-void drawBottomCylinder(void) {
-    GLUquadricObj *obj = gluNewQuadric();
-    glPushMatrix();
-        glTranslatef(0, -1.0, 0);
-        glPushMatrix();
-            GLfloat orange[] = { 1.0, 0.47, 0.0, 1.0 };
-            glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, orange);
-            glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, orange);
-            glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
-            glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 20.0);
-            glRotatef(90, 1.0, 0, 0);
-            gluDisk(obj, 0.0, 1.0, 30, 30);
-            gluCylinder(obj, 1.0, 1, 3, 30, 30);
-        glPopMatrix();
-        glPushMatrix();
-            glPushMatrix();
-                glTranslatef(0, -3.0, 0);
-                glRotatef(-90, 1.0, 0, 0);
-                gluDisk(obj, 0.0, 1.0, 30, 30);
-                trail_index++;
-                vector<float> tmp (16);
-                trail.push_back(tmp);
-                glGetFloatv(GL_MODELVIEW_MATRIX, &trail[trail_index][0]);
-            glPopMatrix();
-        glPopMatrix();
-    glPopMatrix();
-}
-
-bool keyStates[256];
-bool animate = 1;
-
-void keyPressed (unsigned char key, int /*x*/, int /*y*/)
-{
-    keyStates[key] = true;
-    cout << key << " key pressed"<<endl;
-}
-
-void keyUp (unsigned char key, int /*x*/, int /*y*/)
-{
-    keyStates[key] = false;
-    return;
-}
-
-
-void keyOperations ()
-{
-    if (keyStates[27] == true) {
-        exit(0);
-    } else if (keyStates['q'] == true) {
-        rotate_axis[0]['y'].rotate = rotate_axis[0]['z'].rotate = false;
-        rotate_axis[0]['x'].rotate = true;
-    } else if (keyStates['w'] == true) {
-        rotate_axis[0]['x'].rotate = rotate_axis[0]['z'].rotate = false;
-        rotate_axis[0]['y'].rotate = true;
-    } else if (keyStates['e'] == true) {
-        rotate_axis[0]['x'].rotate = rotate_axis[0]['y'].rotate = false;
-        rotate_axis[0]['z'].rotate = true;
-    } else if (keyStates['b'] == true) {
-        rotate_axis[1]['y'].rotate = rotate_axis[1]['z'].rotate = false;
-        rotate_axis[1]['x'].rotate = true;
-    } else if (keyStates['n'] == true) {
-        rotate_axis[1]['x'].rotate = rotate_axis[1]['z'].rotate = false;
-        rotate_axis[1]['y'].rotate = true;
-    } else if (keyStates['m'] == true) {
-        rotate_axis[1]['x'].rotate = rotate_axis[1]['y'].rotate = false;
-        rotate_axis[1]['z'].rotate = true;
-    } else if (keyStates['s'] == true) {
-        animate = true;
-    } else if (keyStates['x'] == true) {
-        animate = false;
-    }
-}
-
-void drawTrail(){
-    glBegin(GL_LINE_STRIP);
-    GLfloat red[] = { 1.0, 0.0, 0.0, 1.0 };
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, red);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, red);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 80.0);
-    for(int j = 0; j < trail_index; j++){
-        glVertex3f(trail[j][12], trail[j][13], trail[j][14]);
-    }
-    glEnd();
-}
-void increaseAngle() {
-    for (auto& i: rotate_axis) {
-        for (auto& kv: i) {
-            if(kv.second.rotate) {
-                if(kv.first == 'x' || kv.first == 'z') {
-                    if (i['x'].angle + i['z'].angle >= 90 || i['x'].angle + i['z'].angle <= -90) {
-                        kv.second.direction = (kv.second.direction + 1) % 2;
-                    }
-                    if (kv.second.direction == 0) {
-                        kv.second.angle += 1;
-                    } else if (kv.second.direction == 1) {
-                        kv.second.angle -= 1;
-                    }
-                } else {
-                    kv.second.angle = (int)(kv.second.angle + 1) % 360;
-                }
-
-            }
-        }
-    }
-}
 
 void display()
 {
@@ -311,7 +189,6 @@ void init(void)
 {
     initLight();
     glEnable(GL_DEPTH_TEST);
-
 }
 
 int main (int argc, char **argv)
@@ -333,4 +210,137 @@ int main (int argc, char **argv)
 
     glutMainLoop();
     return 0;
+}
+
+void drawSphere(void) {
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, green);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 20.0);
+    glutSolidSphere(1.0, 30, 30);
+}
+
+void drawCylinder(void) {
+    GLUquadricObj *obj = gluNewQuadric();
+        glTranslatef(0, -1.0, 0);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, blue);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, blue);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 20.0);
+    glPushMatrix();
+        glRotatef(90, 1.0, 0, 0);
+        gluDisk(obj, 0.0, 1.0, 30, 30);
+        gluCylinder(obj, 1.0, 1, 3, 30, 30);
+        glPopMatrix();
+    glPushMatrix();
+        glTranslatef(0, -3.0, 0);
+        glPushMatrix();
+            glRotatef(-90, 1.0, 0, 0);
+            gluDisk(obj, 0.0, 1.0, 30, 30);
+        glPopMatrix();
+}
+
+void drawBottomCylinder(void) {
+    GLUquadricObj *obj = gluNewQuadric();
+    glPushMatrix();
+        glTranslatef(0, -1.0, 0);
+        glPushMatrix();
+            GLfloat orange[] = { 1.0, 0.47, 0.0, 1.0 };
+            glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, orange);
+            glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, orange);
+            glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
+            glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 20.0);
+            glRotatef(90, 1.0, 0, 0);
+            gluDisk(obj, 0.0, 1.0, 30, 30);
+            gluCylinder(obj, 1.0, 1, 3, 30, 30);
+        glPopMatrix();
+        glPushMatrix();
+            glPushMatrix();
+                glTranslatef(0, -3.0, 0);
+                glRotatef(-90, 1.0, 0, 0);
+                gluDisk(obj, 0.0, 1.0, 30, 30);
+                trail_index++;
+                vector<float> tmp (16);
+                trail.push_back(tmp);
+                glGetFloatv(GL_MODELVIEW_MATRIX, &trail[trail_index][0]);
+            glPopMatrix();
+        glPopMatrix();
+    glPopMatrix();
+}
+
+
+void keyPressed (unsigned char key, int, int)
+{
+    keyStates[key] = true;
+    cout << key << " key pressed"<<endl;
+}
+
+void keyUp (unsigned char key, int /*x*/, int /*y*/)
+{
+    keyStates[key] = false;
+    return;
+}
+
+
+void keyOperations ()
+{
+    if (keyStates[27] == true) {
+        exit(0);
+    } else if (keyStates['q'] == true) {
+        rotate_axis[0]['y'].rotate = rotate_axis[0]['z'].rotate = false;
+        rotate_axis[0]['x'].rotate = true;
+    } else if (keyStates['w'] == true) {
+        rotate_axis[0]['x'].rotate = rotate_axis[0]['z'].rotate = false;
+        rotate_axis[0]['y'].rotate = true;
+    } else if (keyStates['e'] == true) {
+        rotate_axis[0]['x'].rotate = rotate_axis[0]['y'].rotate = false;
+        rotate_axis[0]['z'].rotate = true;
+    } else if (keyStates['b'] == true) {
+        rotate_axis[1]['y'].rotate = rotate_axis[1]['z'].rotate = false;
+        rotate_axis[1]['x'].rotate = true;
+    } else if (keyStates['n'] == true) {
+        rotate_axis[1]['x'].rotate = rotate_axis[1]['z'].rotate = false;
+        rotate_axis[1]['y'].rotate = true;
+    } else if (keyStates['m'] == true) {
+        rotate_axis[1]['x'].rotate = rotate_axis[1]['y'].rotate = false;
+        rotate_axis[1]['z'].rotate = true;
+    } else if (keyStates['s'] == true) {
+        animate = true;
+    } else if (keyStates['x'] == true) {
+        animate = false;
+    }
+}
+
+void drawTrail(){
+    glBegin(GL_LINE_STRIP);
+    GLfloat red[] = { 1.0, 0.0, 0.0, 1.0 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, red);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, red);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 80.0);
+    for(int j = 0; j < trail_index; j++){
+        glVertex3f(trail[j][12], trail[j][13], trail[j][14]);
+    }
+    glEnd();
+}
+void increaseAngle() {
+    for (auto& i: rotate_axis) {
+        for (auto& kv: i) {
+            if(kv.second.rotate) {
+                if(kv.first == 'x' || kv.first == 'z') {
+                    if (i['x'].angle + i['z'].angle >= 90 || i['x'].angle + i['z'].angle <= -90) {
+                        kv.second.direction = (kv.second.direction + 1) % 2;
+                    }
+                    if (kv.second.direction == 0) {
+                        kv.second.angle += 1;
+                    } else if (kv.second.direction == 1) {
+                        kv.second.angle -= 1;
+                    }
+                } else {
+                    kv.second.angle = (int)(kv.second.angle + 1) % 360;
+                }
+
+            }
+        }
+    }
 }
